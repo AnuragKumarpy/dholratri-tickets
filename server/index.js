f873562092a2a2581d36d18f8d9070f56e74b7fa
Anurag Kumar <144139048+AnuragKumarpy@users.noreply.github.com>
2025-09-07 10:27:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ const uploadPaymentQr = multer({
 // --- Express App Setup ---
 const app = express();
 app.use(helmet());
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '5mb' }));
 app.use(cors({
   origin: [
     'http://localhost:3000',
